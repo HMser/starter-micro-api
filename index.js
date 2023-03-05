8beaa8bf2fa520af49f60a89db8e8a3afdeb91fe
HM <95879255+HMser@users.noreply.github.com>
2023-03-05 17:44:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://truecaller2.p.rapidapi.com/findPhone',
+  params: {phone: '972547332554'},
+  headers: {
+    'X-RapidAPI-Key': '5ad4824f82msh9cc7d5dda81bd47p15854ajsn7630c9bdef0c',
+    'X-RapidAPI-Host': 'truecaller2.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
